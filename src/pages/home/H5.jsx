1df9cbf24132131fa5a5d981ed54884df831f776
guruwangchuk7 <guruwangchuk1234@gmail.com>
2026-01-15 09:54:44 +0600

    Footer update

--- src/pages/home/H5.jsx
+++ src/pages/home/H5.jsx
@@ -3,9 +3,11 @@ import React from 'react'
 function H5() {
     return (
         <div>
-            <div className='h-[100vh] flex justify-center items-center text-nutral-900'>
+            <div className='h-[100vh] flex justify-center items-center text-neutral-900'>
                 <div className='text-center w-fit mx-auto'>
-                    <div className="logo font-bold text-3xl lg:text-7xl">SAIDPIECE <span className='font-light'>STUDIO</span></div>
+                    <div className="logo font-bold text-3xl text-neutral-800 lg:text-7xl" style={{ fontFamily: "century-gothic" }}>
+                        <span style={{ color: "#555555" }} className="font-light">said</span><span>piece</span> <span className="font-light">studio</span>
+                    </div>
                 </div>
             </div>
         </div>
